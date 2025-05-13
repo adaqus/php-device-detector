ad30e46038fed9a2a2c89d6d1578e47303546086
Adam Tulejko <a.tulejko@piwik.pro>
2025-05-13 14:42:23 +0200

    rm cache

--- cache/regex/39/5b636f6e736f6c653530352d616c6c5d5b315d.doctrinecache.php
+++
@@ -1,1 +0,0 @@
-<?php return unserialize('a:2:{s:8:"lifetime";i:0;s:4:"data";s:144:"Retroid Pocket 2Plus|PlayStation ?(2|3|4 Pro|4|5|Portable|Vita)|Dreamcast|OUYA|Nintendo (([3]?DS[i]?)|Wii[U]?|Switch)|Xbox|Archos.*GAMEPAD([2]?)";}');
